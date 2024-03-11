7ce28cf9ecf9c8768a46bb5d7b74870e3537cc18
Joe Portner <joe@material.security>
2024-03-11 15:00:15 -0400

    Add Express app

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const LISTEN_PORT = 8080;
+const app = express();
+
+app.get('/', function (req, res) {
+  res.redirect('/home');
+});
+
+app.get('/home', function (req, res) {
+  res.contentType('text/html').send("<h1>Welcome to Home Page</h1>");
+});
+
+app.listen(LISTEN_PORT, function (err) {
+  if (err) console.log(err);
+  console.log("Server listening on port", LISTEN_PORT);
+});
